af1420c233e8ee46b03b090b730fa19cc25939e5
reichmant <reichmant@users.noreply.github.com>
2018-02-20 03:56:18 -0500

    Update SDL_Render_Main.c
    Cleaned up stuff. Added input options.

--- SDL_Render_Main.c
+++ SDL_Render_Main.c
@@ -18,7 +18,8 @@
 // Function Declarations
 void DrawPixel(int r, int g, int b, int x, int y);
 int ValidateColorRange(int color);
-
+void RenderBackground();
+void WaitForClose();
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -26,79 +27,17 @@ const int SCREEN_HEIGHT = 480;
 
 // Globals
 SDL_Renderer *renderer;
+SDL_Event event;
+SDL_Window *window;
 
 int main(void) {
-    SDL_Event event;
-    SDL_Window *window;
-    int x;
-    int y;
+
+
     /* Setup stuff */
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
 
-    /* Render background */
-    //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); // draw in: black
-    //SDL_RenderClear(renderer); // fill screen with black
-
-    /*
-    for (x = 0; x < SCREEN_WIDTH; x++){
-        for (y = 0; y < SCREEN_HEIGHT/5; y++){
-            DrawPixel(60, 40, 0, x, y);
-        }
-    }
-
-    for (x = 0; x < SCREEN_WIDTH; x++){
-        for (y = SCREEN_HEIGHT/5; y < (SCREEN_HEIGHT/5)*2; y++){
-            DrawPixel(30, 20, 0, x, y);
-        }
-    }
-
-    for (x = 0; x < SCREEN_WIDTH; x++){
-        for (y = (SCREEN_HEIGHT/5)*3; y < (SCREEN_HEIGHT/5)*4; y++){
-            DrawPixel(30, 20, 0, x, y);
-        }
-    }
-
-    for (x = 0; x < SCREEN_WIDTH; x++){
-        for (y = (SCREEN_HEIGHT/5)*4; y < SCREEN_HEIGHT; y++){
-            DrawPixel(60, 40, 0, x, y);
-        }
-    }
-    */
-    //har str[12];
-    //int poo = sprintf(str, "%d", 42);
-    int poo = 32;
-    //printf("%d\n", poo);
-
-    int MAXpartition = 9; // must be odd for proper symmetry
-    int R = 60;
-    int G = 40;
-    int B = 0;
-    int partition;
-    for (partition = 0; partition < MAXpartition+1; partition++){
-        for (x = 0; x < SCREEN_WIDTH; x++){
-            //sprintf( "\n", "%d", ((SCREEN_HEIGHT/MAXpartition)*partition));
-            //sprintf( "\n", "%d", ((SCREEN_HEIGHT/MAXpartition)*partition+1));
-            //printf("\n");
-            for (y = (SCREEN_HEIGHT/MAXpartition)*(partition); y < (SCREEN_HEIGHT/MAXpartition)*(partition+1); y++){
-                poo = ((SCREEN_HEIGHT/MAXpartition)*partition);
-                poo = ((SCREEN_HEIGHT/MAXpartition)*(partition+1));
-                //printf("%d\n", poo);
-                DrawPixel(ValidateColorRange(R), ValidateColorRange(G), ValidateColorRange(B), x, y);
-            }
-        }
-        if (partition > (MAXpartition/2)-1){
-            R = R + 5;
-            G = G + 5;
-            B = B + 5;
-        }
-        else{
-            R = R - 5;
-            G = G - 5;
-            B = B - 5;
-        }
-
-    }
+    RenderBackground();
 
     //*****************************************************************************************//
     // The major part of the rendering will be done here.
@@ -113,21 +52,39 @@ int main(void) {
     //getRaycastData();
 
 
+
+
+
+
+
+
+    int squareDim = 90;
+    int squareR = 128;
+    int squareG = 128;
+    int squareB = 128;
+    int x;
+    int y;
+
+    for (x = (SCREEN_WIDTH/2)-(squareDim/2); x < ((SCREEN_WIDTH/2)-(squareDim/2))+(squareDim); x++){
+            for (y = (SCREEN_HEIGHT/2)-(squareDim/2); y < ((SCREEN_HEIGHT/2)-(squareDim/2))+(squareDim); y++){
+                DrawPixel(ValidateColorRange(squareR), ValidateColorRange(squareG), ValidateColorRange(squareB), x, y);
+            }
+        }
+
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_RIGHT]) {
+        printf("<RETURN> is pressed.\n");
+    }
+
+
+
+
+
     /* Update screen */
     SDL_RenderPresent(renderer);
 
-
-    /* Close button functionality */
-    // If you make it here, we're just waiting for input on the X button
-    while (1) {
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            break;
-    }
-    // The actual closing
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return EXIT_SUCCESS;
+    WaitForClose();
+    
 }
 
 /* Draw function abstraction */
@@ -143,5 +100,64 @@ int ValidateColorRange(int color){
     return color;
 }
 
+void RenderBackground(){
+    int currentPartition;
+    int MAXpartition = 9; // must be odd for proper symmetry
+    int R = 60;
+    int G = 40;
+    int B = 0;
+    int x;
+    int y;
+    
+    for (currentPartition = 0; currentPartition < MAXpartition+1; currentPartition++){
+        for (x = 0; x < SCREEN_WIDTH; x++){
+            for (y = (SCREEN_HEIGHT/MAXpartition)*(currentPartition); y < (SCREEN_HEIGHT/MAXpartition)*(currentPartition+1); y++){
+                DrawPixel(ValidateColorRange(R), ValidateColorRange(G), ValidateColorRange(B), x, y);
+            }
+        }
+        if (currentPartition > (MAXpartition/2)-1){
+            R = R + 5;
+            G = G + 5;
+            B = B + 5;
+        }
+        else{
+            R = R - 5;
+            G = G - 5;
+            B = B - 5;
+        }
 
+    }
+}
+
+/* Close button functionality */
+void WaitForClose(){
+    // If you make it here, we're just waiting for input on the X button
+int quit = 0;
+while( !quit ){
+            /* Poll for events */
+            while( SDL_PollEvent( &event ) ){    
+                switch( event.type ){
+                    /* Keyboard event */
+                    /* Pass the event data onto PrintKeyInfo() */
+                    case SDL_KEYDOWN:
+                    case SDL_KEYUP:
+                        quit = 1;
+                        break;
+
+                    /* SDL_QUIT event (window close) */
+                    case SDL_QUIT:
+                        quit = 1;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    // The actual closing
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
 //??? getRaycastData()
